efbb344d15323aa6078e1fc148a7239461911c5c
醉月思 <liqs@dgut.edu.cn>
2019-04-01 12:00:01 +0800

    修复分页BUG

--- TmWeBlog/Action.php
+++ TmWeBlog/Action.php
@@ -5,6 +5,11 @@ class TmWeBlog_Action extends Typecho_Widget implements Widget_Interface_Do {
   private $res;
   public function __construct($request, $response, $params = NULL) {
         parent::__construct($request, $response, $params);
+    	
+     	$this->appID = Typecho_Widget::widget('Widget_Options')->plugin('TmWeBlog')->appID;
+    	$this->appSecret = Typecho_Widget::widget('Widget_Options')->plugin('TmWeBlog')->appSecret;
+        $this->avatarUrl = Typecho_Widget::widget('Widget_Options')->plugin('TmWeBlog')->avatarUrl;
+    	$this->pageSize = Typecho_Widget::widget('Widget_Options')->plugin('TmWeBlog')->pageSize;
         $this->db  = Typecho_Db::get();
         $this->res = new Typecho_Response();
         if (method_exists($this, $this->request->type)) {
@@ -15,110 +20,78 @@ class TmWeBlog_Action extends Typecho_Widget implements Widget_Interface_Do {
         } else {
             $this->defaults();
         }
-    }
+  }
+  function getPageNum(){
+     $select =  $this->db->select('COUNT(cid) AS Num')->from('table.contents')->where('type = ?', 'post')->where('status = ?', 'publish');
+     $data = $this->db->fetchAll($select);
+     $Num =$data[0]['Num'] / $this->pageSize + 1;
+     $this->export((int)$Num);
+  }
+  // 获取博客总览
+  private function getOverview() {
+    $select   = $this->db->select('COUNT(cid) AS Num')->from('table.contents');
+    $data['posts'] = $this->db->fetchAll($select);
+    $select   = $this->db->select('COUNT(coid) AS Num')->from('table.comments');
+    $data['comments'] = $this->db->fetchAll($select);
+    $select   = $this->db->select('COUNT(mid) AS Num')->from('table.metas')->where('type = ?', 'category');
+    $data['categorys'] = $this->db->fetchAll($select);
+    $select   = $this->db->select('COUNT(mid) AS Num')->from('table.metas')->where('type = ?', 'tag');
+    $data['tags'] = $this->db->fetchAll($select);
+    $this->export($data);
+  }
+  // 获取博客作者信息
   private function getAuthorInfo(){
     $select   = $this->db->select('*')->from('table.users');
     $lists = $this->db->fetchAll($select);
-    $lists[0]['avatarUrl'] = "https://www.thinkmoon.cn/usr/uploads/2018/12/55979974.jpg";
+    $lists[0]['avatarUrl'] = $this->avatarUrl;
 	$this->export($lists);
   }
+  // 获取最近post
   private function getRecentPost(){
-    $pageSize = (int) self::GET('pageSize', 1000);
     $page     = (int) self::GET('page', 1);
-    $offset   = $pageSize * ($page - 1);
-    $select   = $this->db->select('cid', 'title','authorId ', 'created', 'slug','commentsNum', 'views', 'likes')->from('table.contents')->where('type = ?', 'post')->where('status = ?', 'publish')->where('created < ?', time())->order('table.contents.created', Typecho_Db::SORT_DESC)->offset($offset)->limit($pageSize);
+    $offset   = $this->pageSize * ($page - 1);
+    $select   = $this->db->select('cid', 'title','authorId ', 'created', 'slug','commentsNum', 'views', 'likes')->from('table.contents')->where('type = ?', 'post')->where('status = ?', 'publish')->where('created < ?', time())->order('table.contents.created', Typecho_Db::SORT_DESC)->offset($offset)->limit($this->pageSize);
     $posts  = $this->db->fetchAll($select);
     foreach ($posts as $post) {
-        $post['tag'] = $this->db->fetchAll($this->db->select('name')->from('table.metas')->join('table.relationships', 'table.metas.mid = table.relationships.mid', Typecho_DB::LEFT_JOIN)->where('table.relationships.cid = ?', $post['cid'])->where('table.metas.type = ?', 'tag'));
-      	$thumb = "https://www.thinkmoon.cn/usr/themes/armx/img/sj/".mt_rand (1, 8).".jpg";
-        $post['thumb'] = $this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $post['cid']))?$this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $post['cid'])):array(array("str_value"=>$thumb));
-        $result[]    = $post;
-    }
-    $this->export($result);
-  }
-  private function getPostBycid(){
-    if (isset($_GET['cid'])) {
-        $cid = self::GET('cid');
-        $select = $this->db->select('cid', 'title', 'created', 'type', 'slug', 'text','commentsNum')->from('table.contents')->where('type = ?', 'post')->where('status = ?', 'publish')->where('created < ?', time())->where('cid = ?', $cid);
-        //更新点击量数据库
-        $row = $this->db->fetchRow($this->db->select('views')->from('table.contents')->where('cid = ?', $cid));
-        $this->db->query($this->db->update('table.contents')->rows(array('views' => (int)$row['views']+1))->where('cid = ?', $cid));
-        $posts  = $this->db->fetchAll($select);
-        foreach ($posts as $post) {
             $post['tag'] = $this->db->fetchAll($this->db->select('name')->from('table.metas')->join('table.relationships', 'table.metas.mid = table.relationships.mid', Typecho_DB::LEFT_JOIN)->where('table.relationships.cid = ?', $post['cid'])->where('table.metas.type = ?', 'tag'));
+      		$thumb = "https://www.thinkmoon.cn/usr/themes/armx/img/sj/".mt_rand (1, 8).".jpg";
+            $post['thumb'] = $this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $post['cid']))?$this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $post['cid'])):array(array("str_value"=>$thumb));
             $result[]    = $post;
         }
         $this->export($result);
-    }
   }
-  private function post1(){
-        $pageSize = (int) self::GET('pageSize', 1000);
-        $page     = (int) self::GET('page', 1);
-        $authorId = self::GET('authorId', 0);
-        $offset   = $pageSize * ($page - 1);
-        $getpage = self::GET('getpage', 0);
-        $idx     = self::GET('idx',-1);
-        
-        // 根据cid偏移获取文章
-        if (isset($_GET['cid'])) {
+  // 通过cid获取post
+  private function getPostBycid(){
+    if (isset($_GET['cid'])) {
             $cid = self::GET('cid');
-            if($getpage) {
-                $select = $this->db->select('cid', 'title', 'created', 'type', 'slug', 'text','commentsNum')->from('table.contents')->where('type = ?', 'page')->where('status = ?', 'publish')->where('created < ?', time())->order('table.contents.created', Typecho_Db::SORT_DESC)->offset($offset)->limit($pageSize);
-            } else {
-                $select = $this->db->select('cid', 'title', 'created', 'type', 'slug', 'text','commentsNum')->from('table.contents')->where('type = ?', 'post')->where('status = ?', 'publish')->where('created < ?', time())->order('table.contents.created', Typecho_Db::SORT_DESC)->offset($offset)->limit($pageSize);
-            }
-             $select->where('cid = ?', $cid);
+            $select = $this->db->select('cid', 'title', 'created', 'type', 'slug', 'text','commentsNum')->from('table.contents')->where('type = ?', 'post')->where('status = ?', 'publish')->where('created < ?', time())->where('cid = ?', $cid);
             //更新点击量数据库
             $row = $this->db->fetchRow($this->db->select('views')->from('table.contents')->where('cid = ?', $cid));
             $this->db->query($this->db->update('table.contents')->rows(array('views' => (int)$row['views']+1))->where('cid = ?', $cid));
-        }
-        else
-        {
-            //如果不指定具体文章CID，不抓取text
-            $select   = $this->db->select('cid', 'title', 'created', 'type', 'slug','commentsNum')->from('table.contents')->where('type = ?', 'post')->where('status = ?', 'publish')->where('created < ?', time())->order('table.contents.created', Typecho_Db::SORT_DESC)->offset($offset)->limit($pageSize);
-        }
-        // 根据分类或标签获取文章
-        if (isset($_GET['category']) || isset($_GET['tag'])) {
-            $name     = isset($_GET['category']) ? $_GET['category'] : $_GET['tag'];
-            $resource = $this->db->fetchAll($this->db->select('cid')->from('table.relationships')->join('table.metas', 'table.metas.mid = table.relationships.mid', Typecho_Db::LEFT_JOIN)->where('slug = ?', $name));
-            $cids     = array();
-            foreach ($resource as $item) {
-                $cids[] = $item['cid'];
-            }
-            $select->where('cid IN ?', $cids);
-        }
-        if($idx>=0){
-            switch($idx)
-            {
-                case 0:
-                    //浏览量
-                    $select->order('table.contents.views', Typecho_Db::SORT_DESC);
-                    break;
-                case 1:
-                    //评论数
-                    $select->order('table.contents.commentsNum', Typecho_Db::SORT_DESC);
-                    break;
-                case 2:
-                    //点赞数
-                    $select->order('table.contents.likes', Typecho_Db::SORT_DESC);
-                    break;
-                default:
-                    break;
-            }
-        }
-        $posts  = $this->db->fetchAll($select);
-        $result = array();
-        $temp = Typecho_Widget::widget('Widget_Options')->plugin('WeTypecho')->hiddenShare;
-        foreach ($posts as $post) {
-            $post        = $this->widget("Widget_Abstract_Contents")->push($post);
+      $posts  = $this->db->fetchAll($select);
+      foreach ($posts as $post) {
             $post['tag'] = $this->db->fetchAll($this->db->select('name')->from('table.metas')->join('table.relationships', 'table.metas.mid = table.relationships.mid', Typecho_DB::LEFT_JOIN)->where('table.relationships.cid = ?', $post['cid'])->where('table.metas.type = ?', 'tag'));
-            $post['thumb'] = $this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $post['cid']))?$this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $post['cid'])):array(array("str_value"=>"https://api.isoyu.com/bing_images.php"));
-            $post['views'] = $this->db->fetchAll($this->db->select('views')->from('table.contents')->where('table.contents.cid = ?', $post['cid']));
-            $post['likes'] = $this->db->fetchAll($this->db->select('likes')->from('table.contents')->where('table.contents.cid = ?', $post['cid']));
-            $post['showshare'] = $temp;
             $result[]    = $post;
         }
         $this->export($result);
+        }
+    
+  }
+  // 获取AccessToken
+  function getAccessToken(){
+    // 如果 session中存有 access_token 并且未超过有效期 使用session中的access_token
+		if( $_SESSION['access_token'] && $_SESSION['expire_time'] > time()) {
+            //$this->export($_SESSION['access_token']);
+			return $_SESSION['access_token'];
+		}else{
+           $url = sprintf('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s',$this->appID,$this->appSecret);
+           $res = json_decode(file_get_contents($url));
+		   $access_token = $res->access_token;
+			//将获取到的access_token存到session 设置过期时间
+		   $_SESSION['access_token'] = $access_token ;
+		   $_SESSION['expire_time'] = time() + 7000 ;
+			return $access_token ;
+		}
   }
   public function export($data = array(), $status = 200) {
         $this->res->throwJson(array(
