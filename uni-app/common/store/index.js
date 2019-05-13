531b9a9108a0afee76c7cdc213a5e3f8ebc8fde1
醉月思 <liqs@dgut.edu.cn>
2019-05-13 17:55:03 +0800

    尝试使用uni-app框架

---
+++ uni-app/common/store/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import * as API from '../../static/utils/api'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state: {
+		authorInfo:{},
+		openid:''
+	},
+	mutations: {
+		setAuthorInfo(state, authorInfo) {
+			state.authorInfo = authorInfo
+		}
+	},
+	actions: {
+		async getUserOpenId (state) {
+			return await new Promise((resolve, reject) => {
+				if (state.openid) {
+					resolve(state.openid)
+				} else {
+					
+				}
+			})
+		},
+		async getAuthorInfo({commit,state}) {
+			let data = await API.getAuthorInfo()
+			commit('setAuthorInfo',data[0])
+		}
+	}
+})
+
+export default store
---
+++ uni-app/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App'
+
+// 引入自定义导航栏
+import cuCustom from './common/componets/cu-custom.vue'
+Vue.component('cu-custom', cuCustom)
+
+import store from './common/store'
+Vue.prototype.$store = store
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+	store,
+	...App
+})
+app.$mount()
