4cf61c735af29d1e07c60115fc8efd5c795b389c
Chauncey_Li <liqs@dgut.edu.cn>
2020-07-10 15:03:03 +0800

    新增环境变量编译模式

--- uni-app-cli/src/plugins/fly/http.ts
+++ uni-app-cli/src/plugins/fly/http.ts
@@ -1,11 +1,11 @@
 var Fly = require("flyio/dist/npm/wx");
 var fly = new Fly();
 
-fly.config.baseURL = process.env.NODE_ENV === "production" ? "https://www.thinkmoon.cn/api/" : "http://localhost/api/";
+fly.config.baseURL = process.env.VUE_APP_BASE_URL;
 
 fly.config.headers = {
   openid: uni.getStorageSync("openid") || "xxx",
-  apisecret: "xxx",
+  apisecret: process.env.VUE_APP_SECRET,
 };
 
 fly.interceptors.response.use(
