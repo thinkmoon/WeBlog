ff2ed57a34043b5abf953f47c1c6fd85797c6b52
Chauncey_Li <liqs@dgut.edu.cn>
2020-07-13 15:43:17 +0800

    更新重package中读取版本号version

--- uni-app-cli/vue.config.js
+++ uni-app-cli/vue.config.js
@@ -4,16 +4,12 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   configureWebpack: {
     plugins: [
-      new CopyWebpackPlugin([{
-        from: path.join(__dirname, "src/plugins"),
-        to: path.join(
-          __dirname,
-          "dist",
-          process.env.NODE_ENV === "production" ? "build" : "dev",
-          process.env.UNI_PLATFORM,
-          "plugins"
-        ),
-      }]),
+      new CopyWebpackPlugin([
+        {
+          from: path.join(__dirname, "src/plugins"),
+          to: path.join(__dirname, "dist", process.env.NODE_ENV === "production" ? "build" : "dev", process.env.UNI_PLATFORM, "plugins"),
+        }
+      ]),
     ],
   },
 };
