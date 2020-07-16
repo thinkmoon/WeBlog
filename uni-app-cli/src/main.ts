60778a6e5642d836897bb7ab8cb7cf682483d244
Chauncey_Li <liqs@dgut.edu.cn>
2020-07-16 12:02:42 +0800

    修复一些代码书写问题

--- uni-app-cli/src/main.ts
+++ uni-app-cli/src/main.ts
@@ -1,9 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-// @ts-ignore
-// import ComponentOptions from '@dcloudio/types'
-
 import "./plugins";
 
 // 由于uni-app的component实现差异，Vue.componet的使用只能在当前文件下生效
